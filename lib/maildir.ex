483ccca29e010e3b9181fd4b2687ab76d84a1145
Mayeu <m@6x9.fr>
2014-07-08 22:31:39 +0200

    Add a quick reply function, to manipulate the API of Maildir.Filename

--- lib/maildir.ex
+++ lib/maildir.ex
@@ -1,2 +1,18 @@
 defmodule Maildir do
+  @doc """
+  Mark an e-mail identified by its path, as replied
+
+  Return its new path.
+
+      iex> Maildir.reply("/path/1204680122.27c448351529ae6fd8673e346ba5979a5b10a0f2427ff89716782219552.azathoth:2,")
+      "/path/1204680122.27c448351529ae6fd8673e346ba5979a5b10a0f2427ff89716782219552.azathoth:2,R"
+  """
+  def reply(p) do
+    path = Path.split(p)
+    {uniq, info} = List.last(path) |> Maildir.Filename.parse
+    info = Maildir.Filename.update_flags(info, :replied, :add)
+    filename = uniq <> ":" <> info
+    List.replace_at(path, -1, filename)
+    |> Path.join
+  end
 end
--- test/maildir_test.exs
+++ test/maildir_test.exs
@@ -1,5 +1,6 @@
 defmodule MaildirTest do
   use ExUnit.Case
+  doctest Maildir
 
   test "the truth" do
     assert 1 + 1 == 2
