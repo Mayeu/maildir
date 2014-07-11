0cd069b1881f29dcfd911038115657816c60732f
Mayeu <m@6x9.fr>
2014-07-11 23:57:41 +0200

    Make better pattern matching when a message struct is needed

--- lib/maildir/message.ex
+++ lib/maildir/message.ex
@@ -18,31 +18,29 @@ defmodule Maildir.Message do
   Process a message from `new` to `cur`
   """
   def process(path) when is_binary(path) do
-    # TODO: check the file exist
+    # TODO: check that the file exist
     process(to_message(path))
   end
 
-  def process(message) when is_map(message), do: :not_implemented
+  def process(m=%Maildir.Message{}), do: :not_implemented
   def process(_), do: :error
 
   @doc """
   Return the full path of a message
   """
-  def path(message) when is_map(message) do
-    Path.join([message.maildir, Atom.to_string(message.folder), join(message.uniq, message.info)])
+  def path(m=%Maildir.Message{}) do
+    Path.join([m.maildir, Atom.to_string(m.folder), join(m.uniq, m.info)])
   end
 
-  # In case you give the path of the message
-  def path(message) when is_binary(message) do
-    {:error, "Expecting me to return you the path of the message given the path
-    of the message does not make sense."}
+  def path(_) do
+    :error
   end
 
   @doc """
   Return the name of a message
   """
-  def filename(message) when is_map(message) do
-    join(message.uniq, message.info)
+  def filename(%Maildir.Message{uniq: uniq, info: info}) do
+    join(uniq, info)
   end
 
   def filename(path) when is_binary(path) do
