c1514baa2c271b729380e5a58fbf9dc2138f143f
Mayeu <m@6x9.fr>
2014-08-10 21:54:42 +0200

    Add some helper functions for the tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,17 @@
 ExUnit.start()
+
+defmodule HelperTest do
+  # Helper that return a temporary directory
+  def mktemp_d do
+    :os.cmd('mktemp -d')
+    |> :erlang.iolist_to_binary
+    |> String.rstrip
+  end
+
+  # Helper that return a temporary file
+  def mktemp do
+    :os.cmd('mktemp')
+    |> :erlang.iolist_to_binary
+    |> String.rstrip
+  end
+end
