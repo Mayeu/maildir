fe94e4ab442a37d05d42612e64156b1184387e35
Mayeu <m@6x9.fr>
2014-08-11 23:47:08 +0200

    Implement the Maildir.open function

--- lib/maildir.ex
+++ lib/maildir.ex
@@ -1,14 +1,16 @@
 defmodule Maildir do
 
+  ## Public API
+
   @doc """
   Simple function to ensure that a maildir exist and contains the three needed
   folder (`new`, `cur`, `tmp`)
 
-  It is not needed to use it, since it just check that a maildir exist, nothing
-  more.
+  It is not needed (but better) to use it, since it just check that a maildir
+  exist, nothing more.
   """
   def open(path) do
-    :not_implemented
+    folders_exist(path, ["new", "cur", "tmp"])
   end
 
   @doc """
@@ -46,4 +48,15 @@ defmodule Maildir do
   def list(maildir, :new), do: :not_implemented
   def list(maildir, :cur), do: :not_implemented
   def list(_, _), do: :error
+
+  ## Private API
+
+  # Test if a list of folders exist inside a certain path
+  defp folders_exist(_, []), do: :ok
+  defp folders_exist(path, [h|t]) do
+    case File.exists?(path <> "/" <> h) do
+      true -> folders_exist(path, t)
+      _ -> {:error, "#{path <> "/" <> h} does not exist"}
+    end
+  end
 end
--- test/maildir_test.exs
+++ test/maildir_test.exs
@@ -14,4 +14,37 @@ defmodule MaildirTest do
     assert File.dir?(maildir <> "/tmp")
     assert File.dir?(maildir <> "/cur")
   end
+
+  test "open should detect if a folder is not a maildir" do
+    # This path does not exist
+    not_maildir = "/this/path/does/not/exist/unless/your/system/is/really/weird"
+    assert {:error, _} = Maildir.open(not_maildir)
+
+    # Test an empty temporary folder
+    not_maildir = HelperTest.mktemp_d()
+    assert {:error, _} = Maildir.open(not_maildir)
+
+    # A folder with a random folder inside
+    not_maildir = HelperTest.mktemp_d()
+    File.mkdir!(not_maildir <> "/random_folder_name")
+    assert {:error, _} = Maildir.open(not_maildir)
+
+    # Now, a lot of test with various folder combination
+    Enum.map(HelperTest.combos(["new", "tmp", "cur"]),
+    fn (folders) ->
+      # Get a tmp folder
+      maildir = HelperTest.mktemp_d()
+      # Create all the needed folder inside
+      Enum.map(folders, fn (folder) -> File.mkdir!(maildir <> "/" <> folder) end)
+
+      # If we are in the case of 3 folder, test that open work, otherwise,
+      # test it does not
+      case length(folders) do
+        3 ->
+          assert :ok = Maildir.open(maildir)
+        _ ->
+          assert {:error, _} = Maildir.open(maildir)
+      end
+    end)
+  end
 end
