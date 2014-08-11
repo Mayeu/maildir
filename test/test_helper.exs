de8bbe44ca6c006db6d4429e134c4fe9280f33fa
Mayeu <m@6x9.fr>
2014-08-11 23:46:30 +0200

    Add an helper fonction that return the combination of all list element

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -14,4 +14,17 @@ defmodule HelperTest do
     |> :erlang.iolist_to_binary
     |> String.rstrip
   end
+
+  # Combination for list, taken from: http://panduwana.wordpress.com/2010/04/21/combination-in-erlang/
+  def combos(1, list) do
+    for e <- list, do: [e]
+  end
+  def combos(k, list) when k == length(list), do: [list]
+  def combos(k, [head | tail]) do
+    (for sub <- combos(k-1, tail), do: [head|sub]) ++ combos(k, tail)
+  end
+
+  def combos(list) do
+    List.foldl(:lists.seq(1,length(list)), [], fn (k, acc) -> acc ++ combos(k, list) end)
+  end
 end
